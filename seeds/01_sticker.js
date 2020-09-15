b87689f108a935395825c038e01a74f40ff950fb
Eduardo <jeduardorjx@gmail.com>
2020-09-14 19:34:02 -0600

    seed database

--- seeds/01_sticker.js
+++ seeds/01_sticker.js
@@ -1,6 +1,8 @@
+const stickers = require('../stickers');
+
 exports.seed = function(knex) {
 	// Deletes ALL existing entries
-	return knex('stickers').del().then(() => {
+	return knex('sticker').del().then(function() {
 		return knex('sticker').insert(stickers);
 	});
 };
